798a9e3fdc8e554c7b63c1af22fe723dcacec1e9
smithcorbin <smith_c11@denison.edu>
2015-11-25 15:25:47 -0600

    Created basic underlying register containers.

--- denapp/client/agent/register/register.js
+++ denapp/client/agent/register/register.js
@@ -1,4 +1,123 @@
+Meteor.subscribe('users');
+
+Template.register.rendered = function() {
+
+    $('#register-confirm-password').addClass("invalid");
+    $('#register-password').addClass("invalid");
+    $('#register-email').addClass("invalid");
+    $('#register-lName').addClass("invalid");
+    $('#register-fName').addClass("invalid");
+
+    uFlag = false;
+    eFlag = false;
+    pFlag = false;
+    cFlag = false;
+    pwFlag = false;
+
+    $('#register-email').on('input', function(){
+	var input=$(this).val();
+	var re = [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b;
+	var Notused = Meteor.users.find({"profile.oh": input }).count() == 0;
+	oh = re.test(input);
+	eFlag = oh && Notused;
+	if(eFlag){
+	    $(this).removeClass("invalid").addClass("valid");
+	}else{
+	    $(this).removeClass("valid").addClass("invalid");
+	    if(!Notused){
+		$('#notif').html("This Email is Already in Use");
+		document.getElementById('notif').style.opacity='1.0'
+		document.getElementById('notif').style.visibility='visible'
+	    setTimeout(function(){
+	      $('#notif').animate({ opacity: 0 }, 1000, 'linear')
+	    }, 3000);
+	    }
+	}
+    });
+
+
+    $('#register-password').on('input', function(){
+	var input=$(this).val();
+	var input2=$('#register-confirm-password').val();
+
+	pwFlag = (input === input2) && input.length > 0;
+
+	if(pwFlag){
+	    $('#register-confirm-password').removeClass("invalid").addClass("valid");
+	    $(this).removeClass("invalid").addClass("valid");
+	}else{
+	    $('#register-confirm-password;).removeClass("valid").addClass("invalid");
+	    $(this).removeClass("valid").addClass("invalid");
+	    if(input.length > 0){
+		$('#notif').html("Passwords Do Not Match");
+		document.getElementById('notif').style.opacity='1.0'
+		document.getElementById('notif').style.visibility='visible'
+	    setTimeout(function(){
+	      $('#notif').animate({ opacity: 0 }, 1000, 'linear')
+	    }, 3000);
+	    }
+	}
+    });
+
+    $('#signup-confirm-password').on('input', function(){
+	var input=$(this).val();
+	var input2 = $('#register-password').val();
+
+	pwFlag = (input === input2) && input.length > 0;
+
+	if(pwFlag){
+	    $(this).removeClass("invalid").addClass("valid");
+	    $('#register-password').removeClass("invalid").addClass("valid");
+	}else{
+	    $(this).removeClass("valid").addClass("invalid");
+	    $('#register-password').removeClass("valid").addClass("invalid");
+	    if(input.length > 0){
+		$('#notif').html("Passwords Do Not Match");
+		document.getElementById('notif').style.opacity='1.0'
+		document.getElementById('notif').style.visibility='visible'
+	    setTimeout(function(){
+	      $('#notif').animate({ opacity: 0 }, 100, 'linear')
+	    }, 3000);
+	    }
+	}
+    }); 
+
 Template.register.events({
+	"click #register-form": function(event, template) {
+	  event.preventDefault();
+		if(uFlag && eFlag && pFlag && cFlag && pwFlag){
+	
+		Accounts.createUser({
+		email: template.find("#register-email").value,
+		password: template.find("#register-password").value,
+
+		}, function(error) {
+		  if (!error) {
+		    Router.go('thanks');
+		  }
+		});
+		
+		}else{
+		    if(!eFlag){
+			$('#notif').html("Please Provide a Valid Email");
+			document.getElementById('notif').style.opacity='1.0'
+			document.getElementById('notif').style.visibility='visible'
+		    setTimeout(function(){
+		      $('#notif').animate({ opacity: 0 }, 1000, 'linear')
+		    }, 3000);
+		    }
+
+		    if(!pwFlag){
+		      $('#notif').html("Please Create and Confirm your Password");
+		      document.getElementById('notif').style.opacity='1.0'
+		      document.getElementById('notif').style.visibility='visible'
+		    setTimeout(function(){
+		      $('#notif').animate({ opacity: 0 }, 1000, 'linear')
+		    }, 3000);
+		    }
+		}
+	},
+
 	"click #back": function(event, instance) {
 		Router.go("back");
 	}
